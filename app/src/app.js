9528bf370b6aba6ac55dc7c1188dc4e6162b83fe
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-01-04 18:32:12 +0530

    NRI-ify stage two

--- app/src/app.js
+++ app/src/app.js
@@ -9,16 +9,7 @@ import AppController from 'src/AppController';
 import Users from 'src/users/Users';
 
 export default angular.module( 'starter-app', [ 'ngMaterial', Users.name ] )
-  .config(($mdIconProvider, $mdThemingProvider) => {
-    // Register the user `avatar` icons
-    $mdIconProvider
-      .defaultIconSet("./assets/svg/avatars.svg", 128)
-      .icon("menu", "./assets/svg/menu.svg", 24)
-      .icon("share", "./assets/svg/share.svg", 24)
-      .icon("google_plus", "./assets/svg/google_plus.svg", 24)
-      .icon("hangouts", "./assets/svg/hangouts.svg", 24)
-      .icon("twitter", "./assets/svg/twitter.svg", 24)
-      .icon("phone", "./assets/svg/phone.svg", 24);
+  .config(($mdThemingProvider) => {
 
     $mdThemingProvider.theme('default')
       .primaryPalette('indigo')
